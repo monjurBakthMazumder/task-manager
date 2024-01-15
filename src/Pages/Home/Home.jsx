eef4130503caf15b45e213697e5d8093abc5b71b
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-15 18:11:58 +0600

    update home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import Banner from "../../Component/Banner/Banner";
-import Who from "../../Component/Who/Who";
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <Who/>
     </div>
   );
 };
