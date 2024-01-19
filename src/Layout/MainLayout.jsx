87cb33cd55596dc2433581e1c0d0b482c726e22f
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-19 23:19:16 +0600

    removed footer

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Component/Shared/Navbar/Navbar";
-import Footer from "../Component/Shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
-      <Footer/>
     </div>
   );
 };
