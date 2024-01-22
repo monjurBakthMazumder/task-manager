ef61566e1a568e11eedd2117e876c9f5af40d6f0
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-23 00:07:06 +0600

    removes navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Component/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
-      <Navbar />
       <Outlet />
     </div>
   );
