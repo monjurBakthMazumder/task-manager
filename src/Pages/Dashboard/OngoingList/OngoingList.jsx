f63463aa2ac6f2fbb0b1729ded3dd31820146ea9
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-26 00:15:32 +0600

    update todo ongoing and complete page

--- src/Pages/Dashboard/OngoingList/OngoingList.jsx
+++ src/Pages/Dashboard/OngoingList/OngoingList.jsx
@@ -9,16 +9,24 @@ const OngoingList = () => {
       {isPending ? (
         <Loading />
       ) : (
-        <div className="px-5 md:px-10 my-5 md:my-10">
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">
-            Ongoing List {task?.length}
-          </h1>
-          <div>
-            {task?.map((item) => (
-              <Card key={item?._id} item={item} />
-            ))}
-          </div>
-        </div>
+        <>
+          {task?.length < 1 ? (
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold flex justify-center items-center h-screen">
+              No Ongoing task available
+            </h1>
+          ) : (
+            <div className="px-5 md:px-10 my-5 md:my-10">
+              <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">
+                Ongoing List {task?.length}
+              </h1>
+              <div>
+                {task?.map((item) => (
+                  <Card key={item?._id} item={item} />
+                ))}
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
--- src/Pages/Dashboard/ToDoList/ToDoList.jsx
+++ src/Pages/Dashboard/ToDoList/ToDoList.jsx
@@ -9,16 +9,24 @@ const ToDoList = () => {
       {isPending ? (
         <Loading />
       ) : (
-        <div className="px-5 md:px-10 my-5 md:my-10">
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">
-            ToDo list {task?.length}
-          </h1>
-          <div>
-            {task?.map((item) => (
-              <Card key={item?._id} item={item} />
-            ))}
-          </div>
-        </div>
+        <>
+          {task?.length < 1 ? (
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold flex justify-center items-center h-screen">
+              No ToDo task available
+            </h1>
+          ) : (
+            <div className="px-5 md:px-10 my-5 md:my-10">
+              <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">
+                ToDo list {task?.length}
+              </h1>
+              <div>
+                {task?.map((item) => (
+                  <Card key={item?._id} item={item} />
+                ))}
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
